8dc551f65d754e4333c38e9e2734117b104f33b7
sandamil <95046697+sandamil@users.noreply.github.com>
2023-04-29 22:58:45 +0200

    First
    First

---
+++ JavaApp/app/src/main/java/com/example/javaapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.javaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivity(FlutterActivity.createDefaultIntent(this));
+
+    }
+}
